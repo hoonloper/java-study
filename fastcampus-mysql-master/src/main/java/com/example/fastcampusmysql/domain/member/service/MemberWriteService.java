8435634d1adcfc75ea4b74987817c625f0cc04f8
hoonloper <yonghoonj98@gmail.com>
2023-04-10 20:45:19 +0900

    Feat: 회원 이름 변경 내역 저장 구현

--- fastcampus-mysql-master/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ fastcampus-mysql-master/src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -2,6 +2,8 @@ package com.example.fastcampusmysql.domain.member.service;
 
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.entity.MemberNicknameHistory;
+import com.example.fastcampusmysql.domain.member.repository.MemberNicknameHistoryRepository;
 import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberWriteService {
     private final MemberRepository memberRepository;
+    private final MemberNicknameHistoryRepository memberNicknameHistoryRepository;
     public Member register(RegisterMemberCommand command) {
         /*
          * 목표
@@ -26,6 +29,29 @@ public class MemberWriteService {
                 .email(command.email())
                 .birthday(command.birthday())
                 .build();
-        return memberRepository.save(member);
+        var savedMember = memberRepository.save(member);
+        saveMemberNicknameHistory(savedMember);
+        return savedMember;
+    }
+
+    public void changeNickname(Long memberId, String nickname) {
+        /*
+        1. 회원의 이름을 변경
+        2. 변경 내역을 저장한다.
+         */
+        var member = memberRepository.findById(memberId).orElseThrow();
+        member.changeNickname(nickname);
+        memberRepository.save(member);
+
+        saveMemberNicknameHistory(member);
+    }
+
+    private void saveMemberNicknameHistory(Member member) {
+        var history = MemberNicknameHistory
+                .builder()
+                .memberId(member.getId())
+                .nickname(member.getNickname())
+                .build();
+        memberNicknameHistoryRepository.save(history);
     }
 }
