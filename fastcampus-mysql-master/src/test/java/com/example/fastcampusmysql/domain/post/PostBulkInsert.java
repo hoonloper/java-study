95b411a7e60037b74eb79e5780717d63c7b19eda
hoonloper <yonghoonj98@gmail.com>
2023-04-12 21:19:26 +0900

    Feat: 성능 테스트를 위한 게시물 벌크 인서트 구현

---
+++ fastcampus-mysql-master/src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsert.java
@@ -0,0 +1,42 @@
+package com.example.fastcampusmysql.domain.post;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.utill.PostFixtureFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class PostBulkInsertTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void bulkInsert() {
+        var easyRandom = PostFixtureFactory.get(
+                3L,
+                LocalDate.of(2022, 1, 1),
+                LocalDate.of(2023, 4, 4)
+        );
+        int _1man = 10000;
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+        var posts = IntStream.range(0, _1man * 100)
+                .mapToObj(i -> easyRandom.nextObject(Post.class))
+                .toList();
+        stopWatch.stop();
+        System.out.println("객체 생성 시간" +stopWatch.getTotalTimeSeconds());
+
+        var queryStopWatch = new StopWatch();
+
+        queryStopWatch.start();
+        postRepository.bulkInsert(posts);
+        queryStopWatch.stop();
+        System.out.println("Insert 시간" +stopWatch.getTotalTimeSeconds());
+    }
+}
