74dcc76e56a4169e36ff24ee32dd5a430756be93
hoonloper <yonghoonj98@gmail.com>
2023-04-15 19:26:31 +0900

    Feat: 오프셋 기반 페이징 구현

--- fastcampus-mysql-master/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
+++ fastcampus-mysql-master/src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -3,9 +3,13 @@ package com.example.fastcampusmysql.application.controller;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.entity.Post;
 import com.example.fastcampusmysql.domain.post.service.PostReadService;
 import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +29,13 @@ public class PostController {
     @GetMapping("/daily-post-counts")
     public List<DailyPostCount> getDailyPostCounts(@RequestBody DailyPostCountRequest request) {
         return postReadService.getDailyPostCounts(request);
+    }
+
+    @GetMapping("/members/{memberId}")
+    public Page<Post> getPosts(
+            @PathVariable Long memberId,
+            Pageable pageable
+    ) {
+        return postReadService.getPosts(memberId, pageable);
     }
 }
