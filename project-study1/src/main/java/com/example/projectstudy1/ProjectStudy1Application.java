f87a02c88db69018fd88a9c279e9d5430252505b
hoonloper <yonghoonj98@gmail.com>
2023-04-20 18:10:57 +0900

    Setting: 연습을 위한 초기 프로젝트 세팅

---
+++ project-study1/src/main/java/com/example/projectstudy1/ProjectStudy1Application.java
@@ -0,0 +1,14 @@
+package com.example.projectstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStudy1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectStudy1Application.class, args);
+	}
+
+}
---
+++ project-study1/src/test/java/com/example/projectstudy1/ProjectStudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStudy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
