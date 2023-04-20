e99bdcce6eb801897c34bf3b2004c85d06f72f12
hoonloper <yonghoonj98@gmail.com>
2023-04-20 18:27:59 +0900

    Feat: MVC 패턴 예제

---
+++ project-study1/src/main/java/com/example/projectstudy1/controller/StudyController.java
@@ -0,0 +1,14 @@
+package com.example.projectstudy1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudyController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("userName", "myName~!");
+        return "greetings";
+    }
+}
