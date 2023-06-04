dba9823667c645fe1fad7eea3f6577f665f00955
hoonloper <yonghoonj98@gmail.com>
2023-06-04 21:33:15 +0900

    Feat: 어노테이션 매핑 정보 사용

--- helloboot/src/main/java/kephispring/helloboot/HelloController.java
+++ helloboot/src/main/java/kephispring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package kephispring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello") // 1. 클래스 레벨을 먼저 참고하고
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,7 +14,10 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    public String hello(String name) {
+    // @RequestMapping(value = "/hello", method = RequestMethod.GET) 옛날방식
+    @GetMapping // 2. 메소드 레벨을 찾아서 등록한다.
+    @ResponseBody // view 파일을 찾지 않고 바디값으로 응답하겠다는 어노테이션, 아래 주석 해결법. RestController는 생략 가능
+    public String hello(String name) { // String을 리턴하면 view를 가장 먼저 찾으려고 하는데 없어서 404 에러가 발생한다.
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- helloboot/src/main/java/kephispring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/kephispring/helloboot/HellobootApplication.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,21 +24,29 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		// Spring 컨테이너 만들기(Assembler)
 		// 스프링 컨테이너는 오브젝트를 만들어 놓고 재사용하기에 싱글톤 패턴과 유사하다, 즉 싱글턴 레지스터라고 한다.
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		// GenericApplicationContext applicationContext = new GenericApplicationContext(); DispatcherServlet 작업 전 프론트 컨트롤러 작업
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class); // Bean 등록 끝
 		applicationContext.registerBean(SimpleHelloService.class); // 컨트롤러 생성자에 주입한다(Dependency injection)
-		applicationContext.refresh(); // Bean 오브젝트 생성해줌
+		applicationContext.refresh(); // Bean 오브젝트 생성해줌, efresh()는 이름이 좀 헷갈리는데 사실 start()임. 이제 컨테이너가 어떤 빈을 만들지 다 알게됐으니 빈을 모두 생성하고 연결하고 컨테이너로서 동작을 시작하게 하는 메소드
 
 		// 아파치 톰캣 웹서버를 임의로 구현 - 서블릿 등록하기
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+			// 대세 방식, Mapping 정보를 서블릿에 직접 넣는 대신 그 요청을 처리할 처리할 컨트롤러 클래스 안에다 매핑 정보를 직접 넣는 방식
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+			).addMapping("/*");
+
+			/* DispatcherServlet 전환 전 수동으로 컨트롤러 작업
 			// 프론트 컨트롤러 처리
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통 기능
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						/* GET /hello */
+						// GET /hello
 						// name을 가져와서 넣어주는 것, Dto라든지 Bean으로 넘겨주는 것을 바인딩
 						String name = req.getParameter("name");
 
@@ -53,7 +63,7 @@ public class HellobootApplication {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
 				}
-			}).addMapping("/*");
+			}).addMapping("/*"); */
 			/* 단일 서블렛 등록
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
