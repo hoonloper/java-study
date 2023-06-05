a694ec5137a99f5feb9dad0716295c723364d3a7
hoonloper <yonghoonj98@gmail.com>
2023-06-05 18:55:10 +0900

    Feat: API 테스트 코드 작성

--- helloboot/src/test/java/kephispring/helloboot/HellobootApplicationTests.java
+++ helloboot/src/test/java/kephispring/helloboot/HellobootApplicationTests.java
@@ -2,7 +2,7 @@ package kephispring.helloboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// 스프링 부트 기반 테스트라 사용하지 않음
 @SpringBootTest
 class HellobootApplicationTests {
 
