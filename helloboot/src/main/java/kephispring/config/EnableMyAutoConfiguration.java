c6004a3d35d72264f55acb0d8977d4d9d5d8abe0
hoonloper <yonghoonj98@gmail.com>
2023-06-06 14:45:58 +0900

    Feat: 동적인 자동 구성 정보 등록 적용

--- helloboot/src/main/java/kephispring/config/EnableMyAutoConfiguration.java
+++ helloboot/src/main/java/kephispring/config/EnableMyAutoConfiguration.java
@@ -11,6 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) // Type class, interface, enum 등등
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) // 컴포넌트 어노테이션이 붙은 클래스들을 import를 통해 구성정보에 추가할 수 있다.
+// @Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) 컴포넌트 어노테이션이 붙은 클래스들을 import를 통해 구성정보에 추가할 수 있다.
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
