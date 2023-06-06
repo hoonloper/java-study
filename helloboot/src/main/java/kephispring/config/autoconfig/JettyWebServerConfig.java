288dd277884e81693078acc6d5028e34e364236c
hoonloper <yonghoonj98@gmail.com>
2023-06-06 16:18:39 +0900

    Feat: 커스텀 @Conditional 구현

--- helloboot/src/main/java/kephispring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/kephispring/config/autoconfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package kephispring.config.autoconfig;
 
+import kephispring.config.ConditionalMyOnClass;
 import kephispring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -8,19 +9,21 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+//@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
-    }
+//    static class JettyCondition implements Condition {
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
+//        }
+//    }
 }
--- helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,24 +1,27 @@
 package kephispring.config.autoconfig;
 
+import kephispring.config.ConditionalMyOnClass;
 import kephispring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 // @Configuration
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+//@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
     }
 
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
-    }
+//    static class TomcatCondition implements Condition {
+//        @Override
+//        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
+//        }
+//    }
 }
