4daaaf6523c69cd7faef8053ba8670490411dee8
hoonloper <yonghoonj98@gmail.com>
2023-06-06 15:47:29 +0900

    Feat: @Conditional과 Condition

--- helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,14 +3,22 @@ package kephispring.config.autoconfig;
 import kephispring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 // @Configuration
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
