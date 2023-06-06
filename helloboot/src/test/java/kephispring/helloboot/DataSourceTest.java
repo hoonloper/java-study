5e44120dee7f36bc499b087a8eea6b27426a7cb0
hoonloper <yonghoonj98@gmail.com>
2023-06-06 19:51:40 +0900

    Feat: DataSource 자동 구성 클래스 및 Hikari 적용

---
+++ helloboot/src/test/java/kephispring/helloboot/DataSourceTest.java
@@ -0,0 +1,26 @@
+package kephispring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@ExtendWith(SpringExtension.class) // junit 테스트 확장
+@ContextConfiguration(classes = HellobootApplication.class) // 모든 빈 정보를 끌어옴
+@TestPropertySource("classpath:/application.properties")
+public class DataSourceTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void connect() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        connection.close();
+    }
+}
