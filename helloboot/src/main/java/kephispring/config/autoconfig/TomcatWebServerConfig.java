fa14c3b9fe47691471f48657b2a6d4edb0520c13
hoonloper <yonghoonj98@gmail.com>
2023-06-06 18:32:10 +0900

    Feat: 자동 구성에 Environment 프로퍼티 적용

--- helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 
@@ -16,8 +17,10 @@ import org.springframework.util.ClassUtils;
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 커스텀을 위한 어노테이션
-    public ServletWebServerFactory serverFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory serverFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath")); // 모든 API path에 적용됨
+        return factory;
     }
 
 //    static class TomcatCondition implements Condition {
--- helloboot/src/main/java/kephispring/helloboot/WebServerConfiguration.java
+++ helloboot/src/main/java/kephispring/helloboot/WebServerConfiguration.java
@@ -5,12 +5,12 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration { // 사용자 구성 정보로 등록하는 법, 스프링 구성 정보를 대체할 수 있어서 조심해야 함
-    @Bean
-    ServletWebServerFactory customerWebServerFactory() {
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setPort(8081);
-        return serverFactory;
-    }
+//    @Bean
+//    ServletWebServerFactory customerWebServerFactory() {
+//        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//        serverFactory.setPort(8081);
+//        return serverFactory;
+//    }
 }
