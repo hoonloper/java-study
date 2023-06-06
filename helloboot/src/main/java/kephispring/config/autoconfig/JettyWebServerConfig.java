b296e580fab0987da64e093ea1e1e572950278dc
hoonloper <yonghoonj98@gmail.com>
2023-06-06 15:39:31 +0900

    Feat: 스타터와 Jetty 서버 구성 추가

---
+++ helloboot/src/main/java/kephispring/config/autoconfig/JettyWebServerConfig.java
@@ -0,0 +1,14 @@
+package kephispring.config.autoconfig;
+
+import kephispring.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@MyAutoConfiguration
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new JettyServletWebServerFactory();
+    }
+}
--- helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/kephispring/config/autoconfig/TomcatWebServerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 // @Configuration
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
     }
