f929ea9cc5169c5828dddd22c02a22ee5b9640be
hoonloper <yonghoonj98@gmail.com>
2023-06-29 20:26:13 +0900

    챕터1: 티켓 판매 애플리케이션 예제 개선 전

---
+++ object-book-example/src/chapter_1_object_design/ticket_sales_app_before/Invitation.java
@@ -0,0 +1,7 @@
+package chapter_1_object_design.ticket_sales_app_before;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ object-book-example/src/chapter_1_object_design/ticket_sales_app_before/TicketSeller.java
@@ -0,0 +1,13 @@
+package chapter_1_object_design.ticket_sales_app_before;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
