f5c9c00c8ea79d700d422465780f14b74ed50798
hoonloper <yonghoonj98@gmail.com>
2023-06-29 20:47:41 +0900

    챕터1: 티켓 판매 애플리케이션 Audience, Bag, TicketOffice 개선

--- object-book-example/src/chapter_1_object_design/ticket_sales_app_after/Audience.java
+++ object-book-example/src/chapter_1_object_design/ticket_sales_app_after/Audience.java
@@ -7,7 +7,13 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    // public Bag getBag() {
+    //     return bag;
+    // }
+
+    // getBag 메서드 제거와 함께 Bag의 존재를 내부로 캡슐화
+    // 즉 TicketSeller와 Audience 사이의 결합도가 낮아짐
+    public long buy(Ticket ticket) {
+        return bag.hold(ticket);
     }
 }
--- object-book-example/src/chapter_1_object_design/ticket_sales_app_after/TicketSeller.java
+++ object-book-example/src/chapter_1_object_design/ticket_sales_app_after/TicketSeller.java
@@ -16,14 +16,6 @@ public class TicketSeller {
     // 캡슐화의 목적은 변경하기 쉬운 객체를 만드는 것임
     // 캡슐화를 통해 객체 내부로의 접근을 제한하면 객체와 객체 사이의 결합도를 낮출 수 있기 때문에 설계를 좀 더 쉽게 변경 가능
     public void sellTo(Audience audience) {
-        if(audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.sellTicketTo(audience);
     }
 }
