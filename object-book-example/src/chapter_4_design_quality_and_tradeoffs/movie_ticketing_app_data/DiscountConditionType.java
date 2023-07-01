af6823e2913a2cd64b4f4e5ea99536aa0c96b070
hoonloper <yonghoonj98@gmail.com>
2023-07-01 15:15:59 +0900

    챕터4: 영화 예매 애플리케이션 데이터 중심 설계

---
+++ object-book-example/src/chapter_4_design_quality_and_tradeoffs/movie_ticketing_app_data/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package chapter_4_design_quality_and_tradeoffs.movie_ticketing_app_data;
+
+public enum DiscountConditionType {
+    SEQUENCE, // 순번 조건
+    PERIOD    // 기간 조건
+}
---
+++ object-book-example/src/chapter_4_design_quality_and_tradeoffs/movie_ticketing_app_data/MovieType.java
@@ -0,0 +1,7 @@
+package chapter_4_design_quality_and_tradeoffs.movie_ticketing_app_data;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT,
+    PERCENT_DISCOUNT,
+    NONE_DISCOUNT
+}
