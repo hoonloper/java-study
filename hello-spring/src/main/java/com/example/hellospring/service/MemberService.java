ce68d9e14f63edd94c48a985520d8dd831cb2e39
hoonloper <yonghoonj98@gmail.com>
2023-08-04 18:03:40 +0900

    Feat: 회원 서비스 개발

---
+++ hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+// 서비스는 비즈니스에 의존적으로 설계 및 개발
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 옵셔널을 바로 반환하는 걸 권장하지 않음
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
